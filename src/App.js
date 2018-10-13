cde9dbf299f98b5f355f89fc95abe5f71dec17cd
Bekzat Omrzakov <bekzat.omrzakov@walmartlabs.com>
2018-10-12 23:06:39 -0700

    functional component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from "./Person/Person";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Hello I'm React App</h1>
+        <p>This is really working you know</p>
+        <Person />
       </div>
     );
   }
---
+++ src/Person/Person.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const person = () => {
+  return <h3>This is imported paragraph.</h3>
+}
+
+export default person;
